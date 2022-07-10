8a1dcd91a8cb129263341487e36b3b20e6b4aa0a
CyberAstronaut101 <emason@masoncomputers.com>
2022-07-10 16:05:53 -0500

    added inital Angular boilerplate

---
+++ frontend/src/app/recipe-dice-roll/recipe-dice-roll.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecipeDiceRollRoutingModule } from './recipe-dice-roll-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RecipeDiceRollRoutingModule
+  ]
+})
+export class RecipeDiceRollModule { }
---
+++ frontend/src/app/recipe-management/recipe-management.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecipeManagementRoutingModule } from './recipe-management-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RecipeManagementRoutingModule
+  ]
+})
+export class RecipeManagementModule { }
